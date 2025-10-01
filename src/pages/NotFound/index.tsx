24de89099f008af439b0b7568e922a6c52f2df1c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-01 14:32:09 -0300

    ✨ feat: cria página NotFound
    Adiciona o componente NotFound com mensagem de erro, imagem ilustrativa e botão de navegação para retornar à página inicial.

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router'
+
+const NotFound = () => {
+    const navigate = useNavigate()
+
+    return (
+        <div className="flex-1 flex flex-col items-center justify-center mt-10 mb-40">
+            <h2 className="text-center text-[#002F52] text-[32px]">
+                Oops! Não encontramos nada por aqui.
+            </h2>
+            <img
+                src="/not_found.webp"
+                alt="sem resultado"
+                className="w-1/2 max-w-[500px] mx-auto mt-4"
+            />
+            <button
+                className="py-3 w-1/4 bg-[#EB9B00] hover:opacity-80 rounded-md shadow-md"
+                onClick={() => navigate('/')}
+            >
+                <h3 className="text-white text-lg font-medium">
+                    Voltar ao Inicio
+                </h3>
+            </button>
+        </div>
+    )
+}
+
+export default NotFound
